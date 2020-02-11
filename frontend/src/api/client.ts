c5acf7f537044a8151f9b4c49f32ef37ca9be3c9
e.grebenshikov <evgreb@gmail.com>
2020-02-11 15:36:44 +0300

    update api client

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,3 +1,11 @@
+import { Client} from "@KirillAmurskiy/socialnetwork-client";
+
 import { getPublicRuntimeConfig } from "../utils/runtimeConfig";
+import unfetch from "isomorphic-unfetch";
 
 const publicConfig = getPublicRuntimeConfig();
+const apiClient = new Client(publicConfig.apiUrl, {
+  fetch: unfetch
+});
+
+export default apiClient;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainLayout from "../components/mainLayout/mainLayout";
 import BaselineGrid from "../components/baselineGrid/baselineGrid";
 import {NextPage} from "next";
 import { getPublicRuntimeConfig } from "..//utils/runtimeConfig";
 
-const Index: NextPage<{ number: number, apiUrl: string }> = ({ number, apiUrl }) => {
+import apiClient from "../api/client";
+import { UserDto } from "@KirillAmurskiy/socialnetwork-client";
+
+const Index: NextPage = ({  }) => {
+  const [users, setUsers] = useState<UserDto[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const users = await apiClient.getUsers(null,null,null,null,null,null,0,10);
+      setUsers(users);
+    };
+    fetch();
+  }, []);
+
   return (
     <MainLayout>
-      <h1>Index page: {apiUrl}</h1>
+      <h1>Count of users: {users.length}</h1>
       <BaselineGrid />
     </MainLayout>
   );
-};
-
-Index.getInitialProps = async (ctx) => {
-  const number = Math.random();
-  const apiUrl = getPublicRuntimeConfig().apiUrl || "/";
-  return { number, apiUrl };
 };
 
 export default Index;
