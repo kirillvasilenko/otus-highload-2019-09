7da3d766514e229df5b2f2b0f42be49f38112d52
e.grebenshikov <evgreb@gmail.com>
2020-02-12 13:30:54 +0300

    add npmrc

--- frontend/src/pages/registration.tsx
+++ frontend/src/pages/registration.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MainLayout from "../components/mainLayout/mainLayout";
+import { useForm } from "react-hook-form";
 
 function Registration(props: React.ComponentProps<any>) {
   return (
