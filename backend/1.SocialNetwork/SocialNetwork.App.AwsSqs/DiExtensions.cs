6f34df8619abe8c095625deb66680ed45f27b033
Kirill Amurskiy <kirill.amurskiy@gmail.com>
2020-02-16 14:37:17 +0300

    add lambda

--- backend/1.SocialNetwork/SocialNetwork.App.AwsSqs/DiExtensions.cs
+++ backend/1.SocialNetwork/SocialNetwork.App.AwsSqs/DiExtensions.cs
@@ -1,6 +1,7 @@
 using Amazon.SQS;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace SocialNetwork.App.AwsSqs
 {
@@ -16,7 +17,8 @@ namespace SocialNetwork.App.AwsSqs
                 .AddScoped<INotificationSender>(c =>
                 {
                     var sqs = c.GetService<IAmazonSQS>();
-                    return new NotificationSenderSqs(sqs, queueUrl);
+                    var logger = c.GetService<ILogger<NotificationSenderSqs>>();
+                    return new NotificationSenderSqs(sqs, logger, queueUrl);
                 });
         }
     }
--- backend/1.SocialNetwork/SocialNetwork.App.AwsSqs/NotificationSenderSqs.cs
+++ backend/1.SocialNetwork/SocialNetwork.App.AwsSqs/NotificationSenderSqs.cs
@@ -1,7 +1,9 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Amazon.SQS;
 using Amazon.SQS.Model;
+using Microsoft.Extensions.Logging;
 using SocialNetwork.Model;
 
 namespace SocialNetwork.App.AwsSqs
@@ -9,17 +11,24 @@ namespace SocialNetwork.App.AwsSqs
     public class NotificationSenderSqs: INotificationSender
     {
         private readonly IAmazonSQS sqs;
+        private readonly ILogger<NotificationSenderSqs> logger;
 
         private string queueUrl;
         
-        public NotificationSenderSqs(IAmazonSQS sqs, string queueUrl)
+        public NotificationSenderSqs(IAmazonSQS sqs, ILogger<NotificationSenderSqs> logger, string queueUrl)
         {
             this.sqs = sqs;
+            this.logger = logger;
             this.queueUrl = queueUrl;
         }
         
         public async Task OnUserRegistered(UserDto user)
         {
+            var str = JsonSerializer.Serialize(sqs.Config, new JsonSerializerOptions(){WriteIndented = true});
+            var tmp = sqs.Config.DetermineServiceURL();
+            logger.LogInformation(str);
+            logger.LogInformation(tmp);    
+            logger.LogInformation(queueUrl);
             var body = JsonSerializer.Serialize(user);
             var request = new SendMessageRequest
             {
