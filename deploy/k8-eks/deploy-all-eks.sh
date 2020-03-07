c4f0a70ac2971b386c7c31a6a75c7ae9b1a09ff4
Kirill Amurskiy <kirill.amurskiy@gmail.com>
2020-03-07 23:13:19 +0500

    add deployment to eks

---
+++ deploy/k8-eks/deploy-all-eks.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+kubectl create secret generic regcred \
+    --from-file=.dockerconfigjson=/C:/Users/kiril/.docker/config1.json \
+    --type=kubernetes.io/dockerconfigjson
+
+kubectl create configmap nginx-config \
+    --from-file=default.conf=nginx/default.conf \
+    --from-file=nginx.conf=nginx/nginx.conf
+
+kubectl apply -f mysql-service.yaml
+kubectl apply -f nginx-service.yaml
+kubectl apply -f socialnetwork-service.yaml
+kubectl apply -f frontend-service.yaml
+
+kubectl apply -f mysql-deployment.yaml
+kubectl apply -f nginx-deployment.yaml
+kubectl apply -f socialnetwork-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
